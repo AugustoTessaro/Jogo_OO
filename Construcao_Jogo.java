849ed745172ccec974fc8acfa39b89929146b9b7
AugustoTessaro <54449309+AugustoTessaro@users.noreply.github.com>
2021-07-12 16:13:24 -0300

    Update Construcao_Jogo.java

--- Construcao_Jogo.java
+++ Construcao_Jogo.java
@@ -2,7 +2,6 @@ package Jogo;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
@@ -12,59 +11,60 @@ import javax.swing.Timer;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.KeyListener;
 
-public class Construcao_Jogo extends JPanel implements ActionListener
+public class Construcao_Jogo extends JPanel implements ActionListener, KeyListener
 {
-	
-	private Image PlanoDeFundo;
-	private Player jogador;
-	private Timer delay;
-	
-	public Construcao_Jogo()
-	{
-		ImageIcon fundo = new ImageIcon("imagem\\"
-				+ "fundo.jpg");
-		PlanoDeFundo = fundo.getImage();
-		
-		jogador = new Player();
-		jogador.esteticaPlayer();
-		
-		addKeyListener(new TecladoAdapter());
-		
-		delay = new Timer(5,this);
-		delay.start();
-	}
 
-	public void paint(Graphics g)
-	{
-		Graphics2D graficos = (Graphics2D) g;
-		graficos.drawImage(PlanoDeFundo, 1, 1, null);
-		graficos.drawImage(jogador.getImagem_nave(), jogador.getX(), jogador.getY(),this);
-		g.dispose();
-	}
-	
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		jogador.atualizar_imagem();
-		repaint();
-	}
-	
-	private class TecladoAdapter extends KeyAdapter
-	{
-		public void KeyPressed(KeyEvent e) 
-		{
-			
-			jogador.KeyPressed(e);
-		}
+    private Image PlanoDeFundo;
+    private Player jogador;
+    private Timer delay;
 
-		public void KeyReleased(KeyEvent e) 
-		{
-			
-			jogador.KeyReleased(e);
-		}
-	}
+    public Construcao_Jogo()
+    {
+        ImageIcon fundo = new ImageIcon("imagem\\planodefundo.jpg");
+        PlanoDeFundo = fundo.getImage();
+
+        jogador = new Player();
+        jogador.esteticaPlayer();
+
+                setFocusable(true);
+        addKeyListener(this);
+
+        delay = new Timer(2,this);
+        delay.start();
+    }
+
+    public void paint(Graphics g)
+    {
+        Graphics2D graficos = (Graphics2D) g;
+        graficos.drawImage(PlanoDeFundo, 1, 1, null);
+        graficos.drawImage(jogador.getImagem_nave(), jogador.getX(), jogador.getY(),this);
+        g.dispose();
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        jogador.atualizar_imagem_player();
+
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) { }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        jogador.KeyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        jogador.KeyReleased(e);
+    }
+
 
 }

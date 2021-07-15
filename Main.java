ecfc59022fdef203dc792f516d4799b2739c4476
AugustoTessaro <54449309+AugustoTessaro@users.noreply.github.com>
2021-07-15 11:29:52 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Jogo;
+
+public class Main 
+{
+	public static void main (String []nargs)    
+	{
+		Tela tela = new Tela();             // Cria uma instancia da Tela, chamando o seu metodo construtor 
+		
+	}
+
+}
---
+++ Movimentos.java
@@ -0,0 +1,10 @@
+package Jogo;
+
+import java.awt.event.KeyEvent;                   // 
+                                                  //
+public interface Movimentos                       // interfaçe de movimentos implementados na classe Player
+{                                                 //
+	public void keyPressed(KeyEvent tecla);       //
+	public void keyReleased(KeyEvent tecla);      //
+}
+

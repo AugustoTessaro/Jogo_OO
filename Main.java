cf139495b01767ee629b98150ed34ca95746a207
AugustoTessaro <54449309+AugustoTessaro@users.noreply.github.com>
2021-07-15 23:04:10 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Jogo;
+
+public class Main 
+{
+	public static void main (String []nargs)    
+	{
+		 new Tela();             // Cria uma instancia da Tela, chamando o seu metodo construtor 
+		
+	}
+
+}
---
+++ Player.java
@@ -0,0 +1,101 @@
+package Jogo;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ImageIcon;
+
+public class Player extends Personagem implements Movimentos 
+{
+    
+    private int dy;
+	private List <Projetil> projeteis; // projetil compoe jogador
+ 
+	
+    public Player()
+    {
+    	super(100, 500);
+        projeteis = new ArrayList<Projetil>();
+    }
+
+    public void atirar()
+    {
+    	this.projeteis.add(new Projetil(x + largura,y + altura/2));
+    }
+    
+    
+    public List<Projetil> getProjeteis() {
+		return projeteis;
+	}
+
+	public void atualizarImagemPlayer()
+    {
+    	if(dy == -3){
+            var testeY = y + dy;
+
+            if(testeY > 0){
+                y = testeY;
+            }
+            else{
+                y = 0;
+            }
+        }
+
+        if(dy == 3){
+            var testeY = y + dy;
+
+            if(testeY + 125 < 1030){
+                y = testeY;
+            }
+            else {
+                y = 905;
+            }
+        }
+    }
+
+
+
+    @Override
+    public void keyPressed(KeyEvent tecla)
+    {
+        int codTecla = tecla.getKeyCode();
+
+        if(codTecla == KeyEvent.VK_W)
+        {
+            dy = -3;
+        }
+
+        if(codTecla == KeyEvent.VK_S)
+        {
+            dy = 3;
+        }
+        
+        if(codTecla == KeyEvent.VK_SPACE)
+        {
+        	atirar();
+        }
+
+    }
+    	
+    @Override
+    public void keyReleased(KeyEvent tecla) 
+    {
+        int codTecla = tecla.getKeyCode();
+
+        if(codTecla == KeyEvent.VK_W || codTecla == KeyEvent.VK_S )
+        {
+            dy = 0;
+        }
+    }
+
+	@Override
+	public void setImagem() {
+		var espaconave = new ImageIcon("imagem\\nave_pequena.png");
+        this.imagem = espaconave.getImage();
+		
+	}
+
+
+	
+	
+}
